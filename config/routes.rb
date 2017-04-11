f962e4f6b998101fb245b6d34c97de3c4c254aeb
Jackson Graziano <jackgraziano@gmail.com>
2017-04-11 15:33:48 -0300

    crud

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  get '/articles/:id/edit', to: 'articles#edit', as: 'edit_article'
+  get '/articles', to: 'articles#index'
+  get '/articles/new', to: 'articles#new', as: 'new_article'
+  get '/articles/:id', to: 'articles#show', as: 'article'
+  post '/articles', to: 'articles#create'
+  patch '/articles/:id', to: 'articles#update'
+  delete '/articles/:id', to: 'articles#destroy'
+
+  # resources :articles
 end
